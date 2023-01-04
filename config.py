fd3532ea8aff3fe911cb383b5b15ff7607377ed2
Devilsbkssks <119072774+Devilsbkssks@users.noreply.github.com>
2023-01-04 15:06:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ IS_PRIVATE = is_enabled(
     os.environ.get("IS_PRIVATE", "False"), "False"
 )  # true for private use and restricting users
 SOURCE_CODE = os.environ.get(
-    "SOURCE_CODE", "https://github.com/kevinnadar22/URL-Shortener-V2"
+    "SOURCE_CODE", "https://github.com/Devilsbkssks/URL-Shortener-V2"
 )  # for upstream repo
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", "")  # image when someone hit /start
 LINK_BYPASS = is_enabled(
